d34f33b4c8533f9022de22900d7678aae3ee5691
Mudar Noufal <mn@mudar.ca>
2015-01-18 13:09:19 -0500

    DetailsActivity

--- ParkCatcherAndroid/app/src/main/java/ca/mudar/parkcatcher/ui/activities/DetailsActivity.java
+++ ParkCatcherAndroid/app/src/main/java/ca/mudar/parkcatcher/ui/activities/DetailsActivity.java
@@ -24,11 +24,24 @@
 package ca.mudar.parkcatcher.ui.activities;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.Window;
+import android.view.WindowManager;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import ca.mudar.parkcatcher.Const;
+import ca.mudar.parkcatcher.ParkingApp;
 import ca.mudar.parkcatcher.R;
 import ca.mudar.parkcatcher.ui.activities.base.ToolbarActivity;
 import ca.mudar.parkcatcher.ui.fragments.DetailsFragment;
+import ca.mudar.parkcatcher.utils.ParkingTimeHelper;
+
 
 public class DetailsActivity extends ToolbarActivity {
     private static final String TAG = "DetailsActivity";
@@ -43,7 +56,7 @@ public class DetailsActivity extends ToolbarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null) {
-            final DetailsFragment fragment = new DetailsFragment();
+            final DetailsFragment fragment = DetailsFragment.newInstance(getIdPostFromIntent());
             getSupportFragmentManager()
                     .beginTransaction()
                     .add(R.id.content_frame, fragment)
@@ -51,9 +64,59 @@ public class DetailsActivity extends ToolbarActivity {
         }
     }
 
-    @Override
-    public void onNewIntent(Intent intent) {
-        setIntent(intent);
-        super.onNewIntent(intent);
+    private int getIdPostFromIntent() {
+        if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
+            return getIdFromUri(getIntent().getData());
+        } else {
+            final Bundle extras = getIntent().getExtras();
+            return extras.getInt(Const.INTENT_EXTRA_POST_ID, Const.UNKNOWN);
+        }
+    }
+
+    private int getIdFromUri(Uri uri) {
+        final List<String> pathSegments = uri.getPathSegments();
+
+        if ((pathSegments.size() == 5)
+                && (pathSegments.get(0).equals(Const.INTENT_EXTRA_URL_PATH_POST_ID))) {
+
+            try {
+                final int postId = Integer.parseInt(pathSegments.get(1));
+                final int dayOfWeekIso = Integer.valueOf(pathSegments.get(2));
+                final double clockTime = Double.valueOf(pathSegments.get(3));
+                final int duration = Integer.valueOf(pathSegments.get(4));
+
+                final int hourOfDay = ParkingTimeHelper.getHoursFromClockTime(clockTime);
+                final int minute = ParkingTimeHelper.getMintuesFromClockTime(clockTime);
+
+                GregorianCalendar calendar = new GregorianCalendar();
+
+                calendar.set(Calendar.DAY_OF_WEEK, ParkingTimeHelper.getDayOfWeek(dayOfWeekIso));
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+
+                final ParkingApp parkingApp = (ParkingApp) getApplicationContext();
+                parkingApp.setParkingCalendar(calendar);
+                parkingApp.setParkingDuration(duration);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return Const.UNKNOWN;
+    }
+
+    public void toggleToobarColor(boolean isForbidden) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            final int colorDark = isForbidden ? getResources().getColor(R.color.theme_alert_primary_dark) : getResources().getColor(R.color.theme_primary_dark);
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(colorDark);
+        }
+        final int color = isForbidden ? getResources().getColor(R.color.theme_alert_primary) : getResources().getColor(R.color.theme_primary);
+        getActionBarToolbar().setBackgroundColor(color);
+
+        final ContextThemeWrapper w = new ContextThemeWrapper(this, isForbidden ? R.style.Theme_AppThemeAlert : R.style.Theme_AppTheme);
+        getTheme().setTo(w.getTheme());
     }
 }
