04c740614ec02b51f49cb0ebe88be99d8fccfd87
Mudar Noufal <mn@mudar.ca>
2015-02-25 11:56:15 -0500

    UIUtils

---
+++ ParkCatcherAndroid/app/src/main/java/ca/mudar/parkcatcher/utils/UIUtils.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/* Modifications:
+ * - Imported from iosched
+ * - Removed all but setAccessibilityIgnore()
+ */
+
+package ca.mudar.parkcatcher.utils;
+
+import android.os.Build;
+import android.view.View;
+
+public class UIUtils {
+    public static void setAccessibilityIgnore(View view) {
+        view.setClickable(false);
+        view.setFocusable(false);
+        view.setContentDescription("");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            view.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_NO);
+        }
+    }
+}
