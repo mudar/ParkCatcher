e2396b25e75c1a422109ca5927c95882129f2de2
Mudar Noufal <mn@mudar.ca>
2017-01-27 16:23:13 -0500

    Footer, added google analytics JS

--- website/public_html/codeigniter/application/views/page.php
+++ website/public_html/codeigniter/application/views/page.php
@@ -64,6 +64,23 @@
   <script src="<?php echo base_url() ?>js/libs/jquery.easing.1.3.js"></script>
   <script src="<?php echo base_url() ?>js/plugins.js"></script>
   <script src="<?php echo base_url() ?>js/script.js<?php echo $assets_version ?>"></script>
+<?php if ( ENVIRONMENT != 'development' ): ?>
+<script>
+// <![CDATA[
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-9867189-5']);
+  _gaq.push(['_setDomainName', 'capteurdestationnement.com']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+// ]]>
+</script>
+<?php endif ?>
 
   <script type="text/javascript">
   // <![CDATA[
